3f1364aa645332f9833b780a071d478500ed5834
Team Eulogik <developers@eulogik.com>
2026-09-10 14:35:28 +0530

    VS Code: repo/homepage metadata, LICENSE, gitignore vsix

---
+++ packages/vscode/out/extension.js
@@ -0,0 +1,64 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+const vscode = __importStar(require("vscode"));
+const child_process_1 = require("child_process");
+const util_1 = require("util");
+const execAsync = (0, util_1.promisify)(child_process_1.exec);
+function activate(context) {
+    const config = vscode.workspace.getConfiguration('opentrustbench');
+    const binPath = config.get('executablePath', 'opentrustbench');
+    async function runCommand(command, args) {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            vscode.window.showErrorMessage('No workspace folder open');
+            return;
+        }
+        const target = await vscode.window.showInputBox({
+            prompt: 'Enter path to scan',
+            value: workspaceFolders[0].uri.fsPath,
+        });
+        if (!target)
+            return;
+        const terminal = vscode.window.createTerminal('OpenTrustBench');
+        terminal.sendText(`${binPath} ${command} "${target}" ${args.join(' ')}`);
+        terminal.show();
+    }
+    context.subscriptions.push(vscode.commands.registerCommand('opentrustbench.scan', () => runCommand('scan', [])), vscode.commands.registerCommand('opentrustbench.trust', () => runCommand('trust', [])), vscode.commands.registerCommand('opentrustbench.attack', () => runCommand('attack', [])));
+}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
